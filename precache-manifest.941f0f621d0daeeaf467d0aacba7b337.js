98e18b0d1b08fcf69ae0b9899d4aae9454f96e7f
lucasdi24 <lucasnndiaz@gmail.com>
2020-10-18 20:23:05 -0300

    Updates

---
+++ precache-manifest.941f0f621d0daeeaf467d0aacba7b337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ad8a113df34badf716a2ebe67763a0",
+    "url": "/EducacionIt-ReactOne/index.html"
+  },
+  {
+    "revision": "12d6e65fb8f1a79a2b3c",
+    "url": "/EducacionIt-ReactOne/static/css/main.8dd85971.chunk.css"
+  },
+  {
+    "revision": "cb90ccc2273b95471e58",
+    "url": "/EducacionIt-ReactOne/static/js/2.16b72719.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/EducacionIt-ReactOne/static/js/2.16b72719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d6e65fb8f1a79a2b3c",
+    "url": "/EducacionIt-ReactOne/static/js/main.383501e2.chunk.js"
+  },
+  {
+    "revision": "500c303a28ea9d480411",
+    "url": "/EducacionIt-ReactOne/static/js/runtime-main.2e7cf9a3.js"
+  }
+]);
